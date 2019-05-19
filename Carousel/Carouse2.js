f4962ab6b9f5b096c7cadc89f9afe9e63d5c1f37
god <1492584282@qq.com>
2019-05-19 09:48:52 +0800

    无缝轮播代码优化

---
+++ Carousel/Carouse2.js
@@ -0,0 +1,68 @@
+let n
+初始化()
+let timer =setInterval(()=>{
+    makeLeave(getImage(n))
+        .one('transitionend',(e)=>{
+            makeEnter($(e.currentTarget))
+        })
+    makeCurrent(getImage(n+1))
+    n += 1
+    },2000)
+document.addEventListener('visibilitychange',function(e){
+    if (document.hidden){
+        window.clearInterval(timer)
+    } else(
+        timer=setInterval(()=>{
+            makeLeave(getImage(n))
+                .one('transitionend',(e)=>{
+                    makeEnter($(e.currentTarget))
+                })
+            makeCurrent(getImage(n+1))
+            n += 1
+        },2000)
+    )
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n){
+    if(n>3){
+        n = n%3
+        if (n===0){
+            n =3
+        }
+    } // n = 1 2 3
+    return n
+}
+
+function 初始化(){
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
+}
